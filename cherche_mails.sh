db1f2467c6d83c40719b0a5d48c71a4725e87917
babidi34 <karimbaidi@gmail.com>
2020-11-15 08:19:24 +0100

    result deviens result.csv

--- cherche_mails.sh
+++ cherche_mails.sh
@@ -3,5 +3,5 @@
 read -p "Entrez le nom du fichier contenant les adresses mails :" fichier
 read -p "Entrez le délimiteur (par exemple ',',':', ' ' ...) :" delimiteur
 
-cat $fichier | awk -F $delimiteur '{ for (i = 1; i <= NF; ++i) if($i ~ "@")print $i}' >> result
-sed -ie 's/"//g' result
+cat $fichier | awk -F $delimiteur '{ for (i = 1; i <= NF; ++i) if($i ~ "@")print $i}' >> result.csv
+sed -ie 's/"//g' result.csv
